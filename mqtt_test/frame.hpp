01de84c44857ca68414449bc800025f6e3197e22
carlosotero2002 <carlos.otero@edu.uag.mx>
2023-03-13 19:12:39 -0600

    frames

--- mqtt_test/frame.hpp
+++ mqtt_test/frame.hpp
@@ -8,3 +8,42 @@ typedef struct {
  uint8_t bCleanS;
  uint16_t bKeepA;
 } fConnect;
+
+typedef struct{
+    uint16_t wLen;
+    char* cName;
+    uint16_t wId;
+} fPublish;
+
+typedef struct{
+    uint8_t bType;
+    uint8_t wLen;
+    uint8_t bFlag;
+    uint8_t bCode;
+}fConnack;
+
+typedef struct{
+    uint16_t wLenA;
+    char* cName;
+    uint8_t bQosA;
+    uint16_t wLenB;
+    uint8_t bQosB;
+}fSubscribe;
+
+typedef struct{
+    uint16_t wLenA;
+    char* cNameA;
+    uint16_t wLenB;
+    char* cNameB;
+}fUnsubscribe;
+
+typedef struct{
+    uint8_t bQosA;
+    uint8_t bQosB;
+    uint8_t bFail;
+}fSuback;
+
+typedef struct{
+    uint8_t bType;
+    uint16_t wLen;
+}fDisconnect
