8bc73530e7f62e5607942ee684baa5401ca58f3c
daniel117622 <dan5jul@hotmail.com>
2023-03-30 01:19:10 +0000

    no

---
+++ mqtt_test/client/client.cpp
@@ -0,0 +1,49 @@
+#include "frame_write.hpp"
+
+#define MAX 80
+#define PORT 64001
+#define SA struct sockaddr
+
+void *worker(void * arg)
+{
+	ClientSocket t1(PORT);
+	t1.Connect();	
+	printf("Sending struct. \n");
+	fConnect sFrame;
+	char name[] = "A long name";
+	writefConnect(&sFrame, name, sizeof(name) , 255);
+	sendfConnect(sFrame, t1);
+	sleep(1); // Wait for server to reach send state.
+	t1.Receive();
+	printf("====================\n");
+	printf("B1: 0x%hx\n", (uint8_t) *(t1.buffer + 0));
+	printf("B2: 0x%hx\n", (uint8_t) *(t1.buffer + 1));
+	printf("B3: 0x%hx\n", (uint8_t) *(t1.buffer + 2));
+	printf("B4: 0x%hx\n", (uint8_t) *(t1.buffer + 3));
+	printf("====================\n");
+
+	sleep(1);
+
+
+	*t1.buffer = 0xC0;
+	*(t1.buffer + 1) = 0x00;
+	t1.Send(t1.buffer, 2);
+	sleep(1);
+	while(1)
+	{
+		t1.Receive();
+		if ((uint8_t)*t1.buffer == 0xD0)
+		{
+			printf("Server pinged back...\n\n");
+		}
+		t1.Send(t1.buffer,69);	
+	}
+} 
+
+int main()
+{
+	pthread_t TID;
+	pthread_create(&TID, NULL,  worker, NULL);
+	pthread_join(TID,NULL);
+	return 0;
+}
---
+++ mqtt_test/server/client_handler.hpp
@@ -0,0 +1,57 @@
+#include <iostream>
+// #define _BSD_SOURCE
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#define BUFFERSIZE 4096
+class ClientSocket
+{
+    public:
+    int sockfd;
+    int status;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFERSIZE];
+
+    ClientSocket(int PORT) 
+    { 
+        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        {
+            printf("\nSocket not created.\n");
+        }
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(64001);
+        if ( inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0 )
+        {
+            printf("\nInvalid address.\n");
+        }
+        int optval = 1;
+        setsockopt(sockfd,SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int));
+                        
+    }
+    void Connect()
+    {
+        
+        if ((status = connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr) )) < 0)
+        {
+            printf("\nConnect failed.\n");
+        }     
+        printf("Connecting to server. \n");  
+    }
+    void Send(char * msg, size_t len)
+    {
+        int res = send(sockfd, msg, len, 0);
+    }
+
+    void Receive()
+    {
+        int res = 0;
+        bzero(buffer, BUFFERSIZE);
+        res = recv(sockfd,buffer,BUFFERSIZE,0);
+    }
+
+};
